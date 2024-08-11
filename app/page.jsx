8d0971471bcbe072e7194243869cf65d40b02b8c
Thomas Ngo <tngo0508@gmail.com>
2024-08-11 14:06:24 -0700

    Learn how to create reusable component

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,12 @@
-import Link from "next/link";
+import Hero from '@/components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 
 const HomePage = () => {
   return (
-    <div className="text-3xl">
-      <h1>Welcome</h1>
-      <Link
-        href={{
-          pathname: "properties",
-          query: { name: "test" },
-        }}
-      >
-        Go to Properties
-      </Link>
-    </div>
+    <>
+      <Hero/>
+      <InfoBoxes/>
+    </>
   );
 };
 
